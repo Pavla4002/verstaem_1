54b8816b1716bb7085b3dc0ba927f7b446fad503
Павла <paha16kot@inbox.ru>
2025-02-18 22:22:26 +0300

    Слайдер верстаем

--- main.js
+++ main.js
@@ -2,17 +2,18 @@ let navLinksBox = document.getElementById('nav-list-links');
 
 let links = [];
 navLinksBox.addEventListener("click", (e)=>{
-    if(e.target && e.target.tagName === 'LI'){
-        links.push(e.target.id);
-
+    e.preventDefault();
+    if(e.target && e.target.tagName === 'A'){
+        links.push(e.target.parentNode.id);
+        console.log(e.target.parentNode.id)
         if(links.length>1){
             document.getElementById(links[0]).classList.remove('active-link');
 
-            e.target.classList.add('active-link');
+            e.target.parentNode.classList.add('active-link');
             
             links.shift()
         }else{
-            e.target.classList.add('active-link');
+            e.target.parentNode.classList.add('active-link');
         }
 
     }
@@ -20,16 +21,28 @@ navLinksBox.addEventListener("click", (e)=>{
 
 // Баннер в первой секции
 
-let bannerSlidesBlocks = document.getElementById('first-section').children[0]
+let bannerSlidesCount = document.getElementById('first-section').children[0].childElementCount
 let slides = [];
+let currentSlide = 1;
 function setSlideBanner(){
-   for (let i = 0; i<bannerSlidesBlocks.childElementCount; i++) {
-       document.getElementById(bannerSlidesBlocks.children[0].id).classList.remove('hidden-slide');
-       slides.push(bannerSlidesBlocks.children[0].id)
-   }
-    setTimeout(()=>{
+    if (currentSlide===bannerSlidesCount){
+        currentSlide-=bannerSlidesCount;
+    }
+    console.log(currentSlide)
+    if (currentSlide===1){
+        document.getElementById('slide-' + currentSlide).classList.remove('hidden-slide');
+    }else{
+        currentSlide+=1
+        document.getElementById('slide-' + (currentSlide-1)).classList.add('hidden-slide');
+        document.getElementById('slide-' + currentSlide).classList.remove('hidden-slide');
+    }
 
-    });
+
+
 }
 
-setSlideBanner();
+
+
+
+
+setInterval(setSlideBanner, 3000, )
